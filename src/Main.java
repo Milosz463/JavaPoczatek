064d09554aa3ca46cd207880247d41ce4ea3710a
Milosz463 <miloszdawiec6@gmail.com>
2024-09-04 13:36:02 +0200

    notatka

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,14 @@ public class Main {
 
         System.out.println("lekcja 1 testgit");
 
+        /**
+         * git config user.name "nick"
+         * git config user.email "mail"
+         * git init
+         * git add .
+         * git commit -m"nazwa"
+         * git push
+         */
+
     }
 }
